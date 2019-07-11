60511c5268b50193222c18c414b37fb4c61f19c5
Layne Faler <laynefaler@gmail.com>
2019-07-11 15:40:37 -0700

    Added space for array and string methods

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const _ = require('lodash');
 const numbers = require('./utils/numbers');
+const strings = require('./utils/strings');
+const arrays = require('./utils/arrays');
 
 
 numbers.forEach(name => { 
@@ -10,3 +12,18 @@ numbers.forEach(name => {
     }
 });
 
+strings.forEach(name => { 
+    String.prototype[name] = function() {
+        const args = arguments;
+        args.unshift(this);
+        return _[name].call(this, args);
+    }
+});
+
+arrays.forEach(name => { 
+    Array.prototype[name] = function() {
+        const args = arguments;
+        args.unshift(this);
+        return _[name].call(this, args);
+    }
+});
